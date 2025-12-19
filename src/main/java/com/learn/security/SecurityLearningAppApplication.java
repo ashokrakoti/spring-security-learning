bdae724f3fdabc5d133b31a415ecbe9df3dc9dcb
ashokrakoti <32910948+ashokrakoti@users.noreply.github.com>
2025-12-19 08:11:56 +0530

    initial commit

---
+++ src/main/java/com/learn/security/SecurityLearningAppApplication.java
@@ -0,0 +1,13 @@
+package com.learn.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityLearningAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityLearningAppApplication.class, args);
+	}
+
+}
