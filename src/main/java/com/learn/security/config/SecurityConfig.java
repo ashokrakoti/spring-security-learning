b1a47b2304765cca46ec887fb523dc4a9cbe0ab4
ashokrakoti <32910948+ashokrakoti@users.noreply.github.com>
2025-12-20 11:55:02 +0530

    changes for basic auth implemented at in memory bean level(level1).

---
+++ src/main/java/com/learn/security/config/SecurityConfig.java
@@ -0,0 +1,62 @@
+package com.learn.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+//Basic auth version with logs and filters
+//    @Bean
+//    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        http
+//                .addFilterBefore((servletRequest, servletResponse, filterChain) -> {
+//                    System.out.println(">>> Before Security Filters");
+//                    filterChain.doFilter(servletRequest, servletResponse);
+//                    System.out.println("<<< After Security Filters");
+//                }, SecurityContextHolderFilter.class)
+//                .csrf(csrf -> csrf.disable())
+//                .logout(logout -> logout.disable())
+//                .authorizeHttpRequests(auth -> auth
+//                .requestMatchers("/public/**").permitAll()
+//                .anyRequest().authenticated()
+//        )
+//                .httpBasic();
+//
+//        return http.build();
+//    }
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/public/**").permitAll()
+                        .requestMatchers("/api/admin", "/api/admin/**").hasRole("ADMIN")
+                        .requestMatchers("/api/hello").hasRole("USER")
+                        .anyRequest().authenticated()
+                ).httpBasic();
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.withUsername("user")
+                .password("{noop}user123")
+                .roles("USER")
+                .build();
+
+        UserDetails admin = User.withUsername("admin")
+                .password("{noop}admin123")
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+
+}
